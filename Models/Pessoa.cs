4f57ae1f3f1d97b7a0394e1dc30bf7b8a438ba76
wesley1248 <wesleytms.es@gmail.com>
2026-07-02 20:58:44 -0300

    Criação da classe Pessoa

---
+++ Models/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Pgvector;
+
+namespace ReconhecimentoFacial.Models
+{
+    public class Pessoa
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [Column(TypeName = "vector(128)")]
+        public Vector Embedding { get; set; }
+
+        public Pessoa()
+        {
+            Nome = string.Empty;
+            Embedding = new Vector(new float[128]);
+        }
+    }
+}
