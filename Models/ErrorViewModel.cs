e01d25b0c00aef462bf8c14e3be6d7040287858a
wesley1248 <wesleytms.es@gmail.com>
2026-07-02 20:20:35 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReconhecimentoFacial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
