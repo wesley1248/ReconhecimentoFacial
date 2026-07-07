48817b08d85a8847cd598bcff8704cfe82f24b5e
wesley1248 <wesleytms.es@gmail.com>
2026-07-06 21:03:31 -0300

    Criando AppDbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReconhecimentoFacial.Models;
+
+namespace ReconhecimentoFacial.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.HasPostgresExtension("vector");
+        }
+    }
+}
